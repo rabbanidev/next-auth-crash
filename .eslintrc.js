fb25d4ecc39ad1426489d16c67c641c05c37398f
rabbanidev <golamrabbani227417@gmail.com>
2024-05-10 23:11:13 +0600

    Next auth crash course (eslint, prettier and husky setup)

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  root: true,
+  extends: [
+    'next',
+    'eslint:recommended',
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['prettier', 'react', 'react-hooks'],
+  rules: {
+    // JavaScript rules
+    'prefer-const': 'warn',
+    'no-var': 'warn',
+    'no-unused-vars': 'warn',
+    'object-shorthand': 'warn',
+    'quote-props': ['warn', 'as-needed'],
+
+    // React rules
+    'react/jsx-fragments': ['warn', 'syntax'], // Shorthand syntax for React fragments
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['js', 'ts', 'jsx', 'tsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
